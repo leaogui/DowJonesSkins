12baf93f18f4e26b952f1a333e946674e53166d4
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-08-25 20:28:59 -0300

    Adicionando nova API Key da Steam

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ app.use(express.static(__dirname))
 // Definindo porta de conexão, podendo ser ou do Heroku ou a 7777
 const PORT = process.env.PORT || 7777
 
+// Trocando API Key da Steam de acordo com a PORT 
+if (PORT == '7777'){
+    key = 'F946785AC15BCE7B5930E5F82AE311CC'
+} else {
+    key = '089C83E667EAEA556A647FCAE86101ED'
+}
+
 // /importando e definindo conexão com a Steam
 var steam   = require('steam-login')
 app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: 'a secret' }))
 app.use(steam.middleware({
 	realm: 'http://localhost:'+PORT+'/', 
 	verify: 'http://localhost:'+PORT+'/'+'verify',
-	apiKey: 'F946785AC15BCE7B5930E5F82AE311CC'}
+	apiKey: key}
 ))
 
 // definindo rotas
