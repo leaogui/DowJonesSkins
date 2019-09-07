2fc6f07dda421f01d0958e0adab94e93a5c96773
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-09-07 14:56:43 -0300

    Corrigindo erros de conexão 9

--- JS/Connections/UserCRUD.js
+++ JS/Connections/UserCRUD.js
@@ -3,17 +3,18 @@ class UserCRUD{
     static signUp(client, json){
 
         let count = 0;
-        client.query('SELECT steamId FROM Usuario WHERE steamId = '+json.steamid+';', (err, res) => {
+        client.query('SELECT steamId FROM Usuario WHERE steamId = "'+json.steamid+'";', (err, res) => {
             console.log(err);
+            console.log(res);
         });
         if (count == 1){
-            client.query('INSERT INTO Usuario VALUES('
-            +json.steamid+', '
-            +json.username+', '
-            +json.profile+', '
-            +json.avatar.small+', '
-            +json.avatar.medium+', '
-            +json.avatar.large+');', (err, res) => {
+            client.query('INSERT INTO Usuario VALUES("'
+            +json.steamid+'", "'
+            +json.username+'", "'
+            +json.profile+'", "'
+            +json.avatar.small+'", "'
+            +json.avatar.medium+'", "'
+            +json.avatar.large+'");', (err, res) => {
                 if (err) throw err;
                 for (let row of res.rows) {
                 console.log(JSON.stringify(row));
