a28460a42a7b255880307dae4fd2c6a02149f764
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-10-04 11:10:25 -0300

    Populando infos do player logado

---
+++ JS/scripts/setUserInfos.js
@@ -0,0 +1,8 @@
+let setUserInfos = (json) => {
+    if (req.session.user){
+        let avatar = document.getElementById('avatar').src = json.avatar.large;
+        let userName = document.getElementById('avatar').src = json.username;
+    }
+};
+
+module.exports = setUserInfos; 
