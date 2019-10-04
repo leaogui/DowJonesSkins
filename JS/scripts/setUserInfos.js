b1e4570be2c64b73eaa52ef5943ca3d8ac7bb1de
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-10-04 11:34:03 -0300

    Retirando mudanças anteriores

--- JS/scripts/setUserInfos.js
+++
@@ -1,8 +0,0 @@
-let setUserInfos = (json, sessao) => {
-    if (sessao){
-        let avatar = document.getElementById('avatar').src = json.avatar.large;
-        let userName = document.getElementById('avatar').src = json.username;
-    };
-};
-
-module.exports = setUserInfos;
--- app.js
+++ app.js
@@ -119,9 +119,7 @@ app.get('/verify', steam.verify(), function(req, res) {
 
     const UserCRUD = require('./JS/Connections/Database/UserCRUD');
     UserCRUD.signUp(client, json);
-    setUserInfos = require('./JS/scripts/setUserInfos.js');
     req.session.user = json;
-    setUserInfos(json, req.session.user);
     res.redirect('/');
 });
  
