285813935d3e41ff79096597a8712afa5f84129f
AndreyLacerda <andrey-lacerda@hotmail.com>
2019-11-08 10:07:50 -0200

    Add query Histórico de Vendas

--- JS/Connections/Database/getHistorico.js
+++ JS/Connections/Database/getHistorico.js
@@ -1,12 +1,14 @@
 async function getHistorico (steamid, client) {
-/*
+
+    const arrayCleaner = require('../../scripts/arrayCleaner');
+
     const queryVendeu = {
-        text: 'SELECT username, nome, foto, h.preco, h.dtvenda FROM historicovendas h INNER JOIN skin s ON s.skinid = h.skinid WHERE vendedorid = ($1) ORDER BY dtvenda DESC;',
+        text: 'SELECT username, nome, foto, r.preco, dtvenda FROM usuario INNER JOIN (SELECT vendedorid, nome, foto, h.preco, dtvenda FROM historicovendas h INNER JOIN skin s on  s.skinid = h.skinid) r ON steamid = vendedorid WHERE vendedorid = ($1) ORDER BY dtvenda DESC;',
         rowMode: 'array'
     }
 
     const queryComprou = {
-        text: 'SELECT username, nome, foto, h.preco, h.dtvenda FROM historicovendas h INNER JOIN skin s ON s.skinid = h.skinid WHERE compradorid = ($1) ORDER BY dtvenda DESC;',
+        text: 'SELECT username, nome, foto, r.preco, dtvenda FROM usuario INNER JOIN (SELECT compradorid, nome, foto, h.preco, dtvenda FROM historicovendas h INNER JOIN skin s on  s.skinid = h.skinid) r ON steamid = compradorid WHERE compradorid = ($1) ORDER BY dtvenda DESC;',
         rowMode: 'array'
     }
 
@@ -14,16 +16,10 @@ async function getHistorico (steamid, client) {
     var res1 = await client.query(queryVendeu, [steamid]);
     resultadoFinal = resultadoFinal.concat(res1.rows);
 
-    var res2 = await client.query(queryComprador, [steamid]);
+    var res2 = await client.query(queryComprou, [steamid]);
     resultadoFinal = resultadoFinal.concat(res2.rows);
-
-    var res3 = await client.query(queryComprou, [steamid]);
-    resultadoFinal = resultadoFinal.concat(res3.rows);
-
-    var res4 = await client.query(queryVendedor, [steamid]);
-    resultadoFinal = resultadoFinal.concat(res4.rows);
     
-    return resultadoFinal;*/
+    return arrayCleaner.arrayCleaner(resultadoFinal);
 }
 
 module.exports.getHistorico = getHistorico;
